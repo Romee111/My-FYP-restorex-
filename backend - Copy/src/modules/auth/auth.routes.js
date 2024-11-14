2d59921ddb22d60c3db64675c24b7edcd8be074f
M. Sohaib Romee <toygamer201@gmail.com>
2024-11-14 16:30:06 +0500

    removed copy

--- backend - Copy/src/modules/auth/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as auth from "./auth.controller.js";
-
-const authRouter = express.Router();
-
-authRouter.post("/signup", auth.signUp);
-authRouter.post("/signin", auth.signIn);
-
-export default authRouter;
